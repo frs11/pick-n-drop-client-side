573cc79d65b5eea7c53238d1d86fcdcba14bbb07
frs11 <fayezurrahman017@gmail.cpm>
2023-12-04 12:16:28 +0600

    added footer

--- src/Layout/UserLayout.jsx
+++ src/Layout/UserLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Shared/Navbar";
+import Footer from "../Components/Shared/Footer";
 
 const UserLayout = () => {
   return (
     <div>
       <Navbar></Navbar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
